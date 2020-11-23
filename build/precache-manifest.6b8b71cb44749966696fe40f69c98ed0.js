1ae26d93b2a31a920c0d2f3a3352b401bf1b61c9
GA-02 <68775681+GA-02@users.noreply.github.com>
2020-11-23 16:25:01 +0300

    Add files via upload

---
+++ build/precache-manifest.6b8b71cb44749966696fe40f69c98ed0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35a03992433128d81b5f21303a39150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "/static/css/main.ae880ad0.chunk.css"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "/static/js/2.54e36cb5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.54e36cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "/static/js/main.8883f8a9.chunk.js"
+  },
+  {
+    "revision": "508c0cc942964afe2594",
+    "url": "/static/js/runtime-main.e5895d71.js"
+  },
+  {
+    "revision": "b528eaf852da05eb9fbb826b13d854d5",
+    "url": "/static/media/roll.b528eaf8.svg"
+  }
+]);
